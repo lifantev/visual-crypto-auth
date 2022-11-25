5271bd52e0004641a48ac02fe4226d550ed0257a
lifantev <lifantev.da@phystech.edu>
2022-11-25 21:49:42 +0300

    fix private imgs tmp storing

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def form_auth():
             return render_template('auth_bad.html')
 
         h_creds = hash(login + password)
-        stored_img = Image.open("./static/storage/{storage[login]}.jpg", "r")
+        stored_img = Image.open(f'./static/storage/{storage[login]}.jpg', 'r')
         # create qr from h_creds
         # check equality of creds using visual_crypto_check(private_image, stored_img, generated_from_creds_qr)
         
@@ -50,28 +50,25 @@ def form_reg():
         if login in storage:
             return render_template('reg_bad.html')
 
-
         h_creds = hash(login + password)
         # create qr from h_creds
         # visual_crypto(qr) -> private_img, stored_img
-        private_img = Image.open("./static/storage/{private_img_name}.jpg", "r")
-
-        # for testing only
-        stored_img = password
-        storage[login] = stored_img
+        
+        private_img_name = login
+        # private_img.save(f'./static/storage/private/{private_img_name}.jpg')
         
         # todo: uncomment
         # storage[login] = login
-        # stored_img.save("./static/storage/{login}.jpg")
+        # stored_img.save(f'./static/storage/{login}.jpg')
 
-        return render_template('reg_ok.html', img_ref = 'storage/{private_img_name}.jpg')
+        return render_template('reg_ok.html', img_ref = f'storage/private/{private_img_name}.jpg')
 
     return render_template('reg.html')
 
 
 @app.route('/iKey/<path:filename>', methods=['GET', 'POST'])
 def iKey(filename):
-    return send_file(filename, as_attachment=True)
+    return send_file(filename, as_attachment=True), os.remove(f'./{filename}')
 
 
 if __name__ == "__main__":
